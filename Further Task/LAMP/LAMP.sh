60228c68518fe46c5e763d6139b847c224cefcdb
ceccs17d16 <43980968+ceccs17d16@users.noreply.github.com>
2019-05-08 20:24:09 +0100

    Update LAMP.sh

--- Further Task/LAMP/LAMP.sh
+++ Further Task/LAMP/LAMP.sh
@@ -6,7 +6,6 @@ sudo systemctl enable apache2
 
 sudo apt-get install mariadb-server mariadb-client
 
-firefox http://localhost/info.php
 sudo mysql_secure_installation
 
 sudo apt install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
