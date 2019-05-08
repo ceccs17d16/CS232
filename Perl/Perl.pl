138ee53f6cbff1e325000840984fcc12762b0278
ceccs17d16 <43980968+ceccs17d16@users.noreply.github.com>
2019-05-08 02:19:47 +0100

    Create Perl.pl

---
+++ Perl/Perl.pl
@@ -0,0 +1,49 @@
+Script started on Wed 01 Jan 2003 01:39:02 AM IST
+]0;s4db2@localhost:~[s4db2@localhost ~]$ cat> [K[K > hello.pl
+# u # /usr/bin/perl
+s use strict: ;
+use warnings;
+
+print"Hello"^[[D    ^[[D            "Hello \n";
+
+[1]+  Stopped                 cat >hello.pl
+]0;s4db2@localhost:~[s4db2@localhost ~]$ perl hello.pl
+Hello 
+]0;s4db2@localhost:~[s4db2@localhost ~]$ cat > ass[K[Kdd.pl
+# /user  r/bin/perl
+
+use strict: ;
+use a warnings: ;
+
+printye   "Enter two numbers: "
+
+$a = <>
+$b = <>
+
+sum   addd s = $a + $b;
+print "sum = add   $adds /n";
+
+[2]+  Stopped                 cat >add.pl
+]0;s4db2@localhost:~[s4db2@localhost ~]$ cat add.pl
+# /usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Enter two numbers: "
+
+$a = <>;
+$b = <>;
+
+my $adds = $a + $b;
+print "sum = $adds /n";
+]0;s4db2@localhost:~[s4db2@localhost ~]$ perl add.pl
+Enter two numbers: 56
+12
+sum = 68 /n]0;s4db2@localhost:~[s4db2@localhost ~]$ 
+]0;s4db2@localhost:~[s4db2@localhost ~]$ logout[K[K[K[K[K[Kexit
+exit
+There are stopped jobs.
+]0;s4db2@localhost:~[s4db2@localhost ~]$ exit
+
+Script done on Wed 01 Jan 2003 01:46:48 AM IST
