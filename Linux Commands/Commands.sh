6d65b553f17886da5ce42e6fea69c18a1ba43eeb
ceccs17d16 <43980968+ceccs17d16@users.noreply.github.com>
2019-05-09 17:07:19 +0100

    Create Commands.sh

---
+++ Linux Commands/Commands.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+mkdir files
+cd files
+cat > commands.txt
+cat > abc.txt
+cat >> abc.txt
+cat abc.txt
+mkdir test
+cat > a.txt
+cat > b.txt
+cat > c.txt
+ls
+ls -l
+ls -lt
+ls -ltr
+ls a* -d
+rm c.txt
+cd ..
+rm test/*
+rmdir test
+
+sort -n abc.txt
+sort -nk7 abc.txt
+sort -r abc.txt
+sort -rnk7 abc.txt
+sort -rn abc.txt
+
+grep -c a.txt b.txt
+cat > pqr.txt
+grep -w 'hello' pqr.txt
+grep -i 'Hello' pqr.txt
+grep -c '1' abc.txt
