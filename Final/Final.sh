4b9b5ed3b17f4864760c09da0033d75b49a221ed
ceccs17d16 <43980968+ceccs17d16@users.noreply.github.com>
2019-05-09 16:53:11 +0100

    Create Final.sh

---
+++ Final/Final.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# OS, version, release number
+lsb_release -a
+
+# kernel version
+uname -r
+
+# list of all available shells
+cat /etc/shells
+
+# show CPU info
+cat /proc/cpuinfo
+
+# show memory info
+cat /proc/meminfo
+
+# show harddisk info 
+sudo hdparm -I /dev/sda 
